34f33fceb943832bce9945334faba902058d5b0f
Anna Liberty <liberty.817@proton.me>
2026-02-13 23:04:52 -0800

    Add doc comments to functions in deck.py and tests
    This makes pylint much happier. Also some other linter fixes.

--- deck.py
+++ deck.py
@@ -1,9 +1,17 @@
+"""
+Defines classes relating to an Uno deck, including proper Deck generation and validating cards can
+be played on top of each other properly. Also defines various types for different kinds of cards
+so they can be validated by the type system.
+"""
 from dataclasses import dataclass
 from enum import Enum, auto
 from random import shuffle
 
 
 class Color(Enum):
+    """
+    The color of an Uno card. Can be red, yellow, blue, or green.
+    """
     RED = auto()
     YELLOW = auto()
     BLUE = auto()
@@ -11,12 +19,22 @@ class Color(Enum):
 
 
 class Deck:
+    """
+    A set of cards and a method to generate cards for a deck.
+    """
     cards = []
 
     def __init__(self):
+        """
+        Creates an empty deck.
+        """
         self.cards = []
 
     def add_default_cards(self):
+        """
+        Adds the default Uno cards (as defined by the rules) to the deck. Does not otherwise
+        modify the deck or remove existing cards.
+        """
         colors = [Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN]
 
         self.cards = []
@@ -38,35 +56,60 @@ class Deck:
 
         shuffle(self.cards)
 
+    def can_play_on_top(self, playing):
+        """
+        Determines whether a card can be played on the top card of the deck.
+        """
+        return can_play_card(self.cards[0], playing)
+
 
 @dataclass
 class Number:
+    """
+    A number card, which has a particular color and number.
+    """
     color: Color
     number: int
 
 
 @dataclass
 class Wild:
+    """
+    A wild card, which may or may not have a color depending on whether it has been played.
+    """
     color: Color | None = None
 
 
 @dataclass
 class DrawFourWild:
+    """
+    A plus four wild card, which may or may not have a color depending on whether it has been
+    played.
+    """
     color: Color | None = None
 
 
 @dataclass
 class Skip:
+    """
+    A skip card, which has a color. Is considered one of the "Special" cards.
+    """
     color: Color
 
 
 @dataclass
 class DrawTwo:
+    """
+    A +2 card, which has a color. Is considered one of the "Special" cards.
+    """
     color: Color
 
 
 @dataclass
 class Reverse:
+    """
+    A reverse card, which has a color. It is considered one of the "Special" cards.
+    """
     color: Color
 
 
@@ -74,6 +117,11 @@ Card = Number | Wild | DrawFourWild | Reverse | Skip | DrawTwo
 
 
 def can_play_card(top: Card, playing: Card) -> bool:
+    """
+    Determines whether or not a card can be played on top of another card according to the UNO
+    rules. Wilds can be played on any card, special cards can be placed on other cards with the
+    same color or type, and number cards can be placed on other cards with the same color or number.
+    """
     if playing == top or (type(top) is type(playing) and type(playing) is not Number):
         return True
 
--- deck_test.py
+++ deck_test.py
@@ -12,34 +12,50 @@ from deck import (
     can_play_card,
 )
 
-
 class TestValidCards(unittest.TestCase):
+    """
+    Makes sure the deck generation and comparison code behave correctly.
+    Uses a new deck for each method.
+    """
     def test_generation(self):
+        """
+        Ensures the generated deck is the correct size.
+        """
         deck = Deck()
         deck.add_default_cards()
         self.assertTrue(len(deck.cards) == 108)
-        
+
     def test_wilds(self):
+        """
+        Ensures wild cards (normal and plus four) can be played on all cards in the deck.
+        """
         deck = Deck()
         deck.add_default_cards()
-        
+
         for card in deck.cards:
             self.assertTrue(can_play_card(card, Wild()))
             self.assertTrue(can_play_card(card, DrawFourWild()))
-                    
+
     def test_identical_cards(self):
+        """
+        Ensures idential cards can always be played on top of each other.
+        """
         deck = Deck()
         deck.add_default_cards()
-        
+
         for card in deck.cards:
             self.assertTrue(can_play_card(card, card))
-                    
+
     def test_special(self):
+        """
+        Ensures all special cards (Skip, Reverse, and DrawTwo) can be played on other cards
+        of the same color or kind.
+        """
         deck = Deck()
         deck.add_default_cards()
-        
+
         kinds = [Skip(Color.BLUE), Reverse(Color.BLUE), DrawTwo(Color.BLUE)]
-        
+
         for kind in kinds:
             for card in deck.cards:
                 if card.color == Color.BLUE:
@@ -50,6 +66,11 @@ class TestValidCards(unittest.TestCase):
                     self.assertFalse(can_play_card(card, kind))
 
     def test_number_cards(self):
+        """
+        Ensures some number cards can be played on top of number cards with the same number or
+        color. Tests important variants: different numbers and different colors, and ensures they
+        can be properly played or not played on top of each other.
+        """
         self.assertTrue(can_play_card(Number(Color.BLUE, 10), Number(Color.RED, 10)))
         self.assertTrue(can_play_card(Number(Color.BLUE, 10), Number(Color.BLUE, 5)))
         self.assertFalse(can_play_card(Number(Color.BLUE, 10), Number(Color.RED, 5)))
